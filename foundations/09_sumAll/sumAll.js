731ebfb869582751693c68edbc1088c8cd6a92f8
paul-leiva <54649136+paul-leiva@users.noreply.github.com>
2026-07-10 08:57:27 -0400

    Javascript: Exercise 09

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(low, high) {
+  // Validate arguments
+  errorReturn = "ERROR";
+  if (!Number.isInteger(low) || low < 0) {
+    return errorReturn;
+  }
+  if (!Number.isInteger(high) || high < 0) {
+    return errorReturn;
+  }
 
+  let aux = low;
+  low = Math.min(low, high);
+  high = Math.max(aux, high);
+  
+  let sumOfIntegers = 0;
+  for (i = low; i <= high; i++) {
+    sumOfIntegers += i;
+  }
+  return sumOfIntegers;
 };
 
 // Do not edit below this line
