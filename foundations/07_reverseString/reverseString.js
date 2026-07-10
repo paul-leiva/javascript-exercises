ba1d91ed7ab3081c8a86ffb85d5098f33f40e7c6
paul-leiva <54649136+paul-leiva@users.noreply.github.com>
2026-07-10 08:19:03 -0400

    Javascript: Exercise 07

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(input) {
+  reversed = ""
+  for (i = input.length - 1; i > -1; i--) {
+    reversed += input.charAt(i);
+  }
 
+  return reversed;
 };
 
 // Do not edit below this line
