f52bdb1b0fc3e592f638f0582d2d6a99f942ddfd
paul-leiva <54649136+paul-leiva@users.noreply.github.com>
2026-07-10 08:47:14 -0400

    Javascript: Exercise 08

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...params) {
+  result = array;
+  for (i = 0; i < params.length; i++) {
+    result = result.filter(res => res !== params[i]);
+  }
+  return result;
 };
 
 // Do not edit below this line
