5073534e3500d513bcb4df8998cefce1a71681ee
paul-leiva <54649136+paul-leiva@users.noreply.github.com>
2026-07-11 18:56:40 -0400

    Javascript Exercise 11

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,23 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fTemp) {
+  let result = Number(((fTemp - 32) * (5/9)).toFixed(1));
+  console.log("initial result: " + result);
+
+  if (result % 0.1 == 0) {
+    result = Number(result.toFixed());
+  }
+
+  return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cTemp) {
+  let result = Number((cTemp * (9/5) + 32).toFixed(1));
+  console.log("initial result: " + result);
+
+  if (result % 0.1 == 0) {
+    result = Number(result.toFixed());
+  }
+
+  return result;
 };
 
 // Do not edit below this line
