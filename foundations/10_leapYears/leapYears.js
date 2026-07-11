30d928e4cd6a774d942cef5cc9462e632e228010
paul-leiva <54649136+paul-leiva@users.noreply.github.com>
2026-07-11 18:32:09 -0400

    Javascript Exercise 10

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,18 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+  // If the year is divisible by 4
+  if (year % 4 == 0) {
+    // If the year is divisible by 100 AND not divisible by 400
+    if (year % 100 == 0 && year % 400 != 0) {
+      // return false
+      return false;
+    }
+    
+    // return true
+    return true;
+  }
+  
+  // return false in all other cases
+  return false;
 };
 
 // Do not edit below this line
