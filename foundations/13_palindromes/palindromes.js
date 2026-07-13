45e7bf05d983483da4314ce2318e07654c8ee9ed
paul-leiva <54649136+paul-leiva@users.noreply.github.com>
2026-07-12 22:24:15 -0400

    Javascript Exercise 13

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,28 @@
-const palindromes = function () {
+const palindromes = function (input) {
+  reverse = "";
 
+  lettersOnly = "";
+  for (let j = 0; j < input.length; j++) {
+    const code = input.charCodeAt(j);
+    if ((code > 47 && code < 58) ||  // numeric (0-9)
+    (code > 64 && code < 91) ||  // upper alpha (A-Z)
+    (code > 96 && code < 123) ) { // lower alpha (a-z)
+      lettersOnly += input.charAt(j);
+    }
+  }
+
+  for (let i = input.length - 1; i > -1; i--) {
+    const code = input.charCodeAt(i);
+    if ((code > 47 && code < 58) ||  // numeric (0-9)
+    (code > 64 && code < 91) ||  // upper alpha (A-Z)
+    (code > 96 && code < 123) ) { // lower alpha (a-z)
+      reverse += input.charAt(i);
+    }
+  }
+
+  console.log("reverse (lowercase): " + `${reverse.toLowerCase()}`);
+
+  return reverse.toLowerCase() === lettersOnly.toLowerCase();
 };
 
 // Do not edit below this line
