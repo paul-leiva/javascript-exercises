176b898ed4ebb9715334eaea2cfb9c13d22414e4
paul-leiva <54649136+paul-leiva@users.noreply.github.com>
2026-07-12 21:43:57 -0400

    Javascript Exercise 12

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(number1, number2) {
+	return number1 + number2;
 };
 
-const subtract = function() {
-	
+const subtract = function(number1, number2) {
+	return number1 - number2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let total = 0;
+  for (const a of array) {
+    total += a;
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let product = 1;
+  for (const a of array) {
+    product *= a;
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	result = 1;
+  while (number > 1) {
+    result *= number;
+    number--;
+  }
+  return result;
 };
 
 // Do not edit below this line
