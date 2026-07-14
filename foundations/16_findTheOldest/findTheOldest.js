f84827df272507c6050bd22df8ff41b293c76b18
paul-leiva <54649136+paul-leiva@users.noreply.github.com>
2026-07-14 08:54:29 -0400

    Javascript Exercise 16

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  let oldestAge = 0;
+  const currentYear = new Date().getFullYear();
+  let oldestPerson = null;
+  for (const person of people) {
+    let age = 0;
+    if (Object.hasOwn(person, "yearOfDeath")) {
+      age = person.yearOfDeath - person.yearOfBirth;
+    }
+    else {
+      age = currentYear - person.yearOfBirth;
+    }
 
+    if (age > oldestAge) {
+      oldestPerson = person;
+      oldestAge = age;
+    }
+  }
+  return oldestPerson;
 };
 
 // Do not edit below this line
