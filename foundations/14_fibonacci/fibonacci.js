d9e9a86cdc8531d19cd01e38ae59956d9df268d7
paul-leiva <54649136+paul-leiva@users.noreply.github.com>
2026-07-14 08:35:03 -0400

    Javascript Exercise 14

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+  if (typeof(number) != Number) {
+    number = Number(number);
+  }
 
+  if (number < 0) {
+    return "OOPS";
+  }
+  
+  if (number == 0) {
+    return 0;
+  }
+  else if (number == 1) {
+    return 1;
+  }
+  else {
+    return fibonacci(number - 2) + fibonacci(number - 1);
+  }
 };
 
 // Do not edit below this line
